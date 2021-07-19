3ae59f9c26bc2f87bfdb33271c4f873e83732923
Lotfi <hl_feraga@esi.dz>
2021-07-19 11:34:30 +0100

    working on it

--- cartoonize_image.py
+++ cartoonize_image.py
@@ -1,4 +1,13 @@
 # image cartoonizer using opencv2
 # importing the required modules
+import cv2 #image processing 
+import easygui #open the filebox
+import numpy as np  #store image and process them as numbers 
+import imageio #read an image from a particular path 
+import sys 
+import matplotlib.pyplot as plot #visualize data 
+import os 
+import tkinter as tk #library to make GUIs 
+from tkinter import *
 
 
